852f16503e85df0b5c6259b18ab54ce898998c44
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-07-28 06:36:28 -0300

    shortened some commands

--- aliases.zsh
+++ aliases.zsh
@@ -19,9 +19,10 @@ alias short='cd; cd .oh-my-zsh/custom; nano aliases.zsh'
 alias shortcuts='short'
 
 ##### GIT
-function add () { git add $@; }
-function commit () { git commit -m $1; }
-function commit.add () { git add $1; git commit -m $2; }
-function commit.alias () { cd;cd .oh-my-zsh/custom;git commit -am $1;git push origin master; }
+function add(){git add $@;}
+function commit(){git commit -m $1;}
+function commit.add(){git add $1;git commit -m $2;}
+function commit.alias(){cd;cd .oh-my-zsh/custom;git commit -am $1;git push origin master;}
 
 ##### OTHER STUFF
+function testt(){echo $1;}
