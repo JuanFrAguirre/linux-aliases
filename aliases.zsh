fee234240867e19b0889e4f8e2ecc1768afa1008
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-07-28 05:49:42 -0300

    updated git functions

--- aliases.zsh
+++ aliases.zsh
@@ -17,6 +17,8 @@ alias projects='cd; cd projects'
 alias play='projects;cd playground'
 
 ##### GIT
-function commit () { git add "$1"; git commit -m "$2"; }
+function add () { git add "$@"; }
+function commit () { git commit -m "$1"; }
+function commit.add () { git add "$1"; git commit -m "$2"; }
 function commit.alias () { cd;cd .oh-my-zsh/custom;git commit -am "$1";git push origin master; }
 function makedir () { mkdir "$@"; }
