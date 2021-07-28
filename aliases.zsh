3c8627117f1b6472f4e0fda8a7fce828c555bdd2
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-07-28 05:44:24 -0300

    updated some git functions and restructured the whole file

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,4 @@
-alias desktop='cd; cd Desktop'
-alias downloads='cd; cd Downloads'
-alias projects='cd; cd projects'
-alias play='projects;cd playground'
+##### GENERAL PURPOSE/PROGRAMS
 alias vsc='code .; exit'
 alias open='xdg-open .'
 alias opex='open;exit'
@@ -13,12 +10,15 @@ alias sudoInstall='sudo dpkg -i'
 alias tarxz='sudo tar -xf'
 alias remove='rm -rf'
 
+##### FILES/FOLDERS
+alias desktop='cd; cd Desktop'
+alias downloads='cd; cd Downloads'
+alias projects='cd; cd projects'
+alias play='projects;cd playground'
+
 ##### GIT
 function commit () { git add "$1"; git commit -m "$2"; }
-function alias_commit () {
-  cd;
-  cd .oh-my-zsh/custom;
-  git commit -am "$1";
-  git push origin master;
-}
+function commit.alias () { cd;cd .oh-my-zsh/custom;git commit -am "$1";git push origin master; }
 function makedir () { mkdir "$@"; }
+function speak () { echo "$1"; }
+function speak.asd () { echo "$1"; }
