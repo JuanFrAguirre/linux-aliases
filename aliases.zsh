3c420f404d891ae2bd85076f28b52b3e257c5c0f
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-07-28 05:37:08 -0300

    added more functions

--- aliases.zsh
+++ aliases.zsh
@@ -5,8 +5,15 @@ alias play='projects;cd playground'
 alias vsc='code .; exit'
 alias open='xdg-open .'
 alias opex='open;exit'
+alias txt='sudo gedit'
 alias short='cd; cd .oh-my-zsh/custom; nano aliases.zsh'
 alias shortcuts='short'
-alias reset='cd; source .zshrc'
+alias reset='cd; source .zshrc; clear'
 alias sudoInstall='sudo dpkg -i'
 alias tarxz='sudo tar -xf'
+alias remove='rm -rf'
+
+##### GIT
+function commit () { git add "$1"; git commit -m "$2"; }
+function alias_commit () { cd;cd .oh-my-zsh/custom; git commit -am "$1"; }
+function makedir () { mkdir "$@"; }
